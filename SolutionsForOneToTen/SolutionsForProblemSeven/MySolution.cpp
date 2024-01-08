1d0b5b6cdc1a8340902249de2d5a573e6fdbbdcf
AmjadLap <amjad.sayed.private@gmail.com>
2024-01-08 17:47:53 +0200

    Finshed Problem Number Six(Another Solution)

---
+++ SolutionsForOneToTen/SolutionsForProblemSeven/MySolution.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message)
+{
+
+    int Number = 0;
+
+
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    }
+    while (Number <= 0);
+    
+    
+    return Number;
+}
+
+int ReverseTheNumbers(int Number)
+{
+    int Remainder = 0, Reverse = 0; 
+
+    while (Number != 0) 
+    {
+        Reverse = Reverse * 10;
+        
+        Remainder = Number % 10;
+        Number = Number/ 10;
+
+        Reverse = Remainder + Reverse;
+
+    }
+
+
+    return Reverse;
+}
+
+
+int main()
+{
+
+    int Number = ReadPositiveNumber("Please Enter A Positive Number? ");
+    
+
+    cout << Number << " Reversed " << ReverseTheNumbers(Number) << endl;
+
+
+    return 0;
+}
