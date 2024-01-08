77cf7078b2e6f1addbfc335ca2f146ed5c4720d4
AmjadPC <elsayedamged9@gmail.com>
2024-01-08 03:40:29 +0200

    Finshed Problem Number Five

--- SolutionsForOneToTen/test.cpp
+++ SolutionsForOneToTen/test.cpp
@@ -5,7 +5,26 @@ using namespace std;
 
 int main()
 {
+    int Number = 0;
+    cout << "enter a number? " << endl;
+    cin >> Number;
 
+
+
+    int  Remiander = 0;
+
+    while (Number != 0)
+    {
+        Remiander = Remiander * 10 + Number % 10;
+
+        Number = Number / 10;
+
+        
+
+
+    }
+        cout << Remiander << endl;
+    
 
 
 
