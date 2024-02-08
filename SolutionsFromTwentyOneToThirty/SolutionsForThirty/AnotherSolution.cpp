c2aa61616f4d35958a307039c4c1cc7012a83c22
AmjadLap <amjad.sayed.private@gmail.com>
2024-02-08 07:35:09 +0200

    :---)

--- SolutionsFromTwentyOneToThirty/SolutionsForThirty/AnotherSolution.cpp
+++ SolutionsFromTwentyOneToThirty/SolutionsForThirty/AnotherSolution.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 int main()
 {
-
+    cout << "\n\t\tSAME LIKE ME SOLUTION :---) \n";
     return 0;
 }
--- SolutionsFromTwentyOneToThirty/SolutionsForThirty/MySolution.cpp
+++ SolutionsFromTwentyOneToThirty/SolutionsForThirty/MySolution.cpp
@@ -1,11 +1,111 @@
+#include <cstdlib>
 #include <iostream>
 
 
 using namespace std;
 
 
-int main()
+int ReadPositiveNumber(string Message)
 {
 
+    int Number = 0;
+
+    do {
+        
+        cout << Message << endl;
+        cin >> Number;
+
+
+    }
+    while (Number <= 0);
+
+    cout << endl;
+
+    return Number;
+}
+
+
+int RandomNumber(int From, int To)
+{
+    int RandomNumber = rand() % (To - From + 1) + From;
+
+    return RandomNumber;    
+}
+
+
+void FillArrayWithRandomNumbers(int arr[100], int ArrayLength) // In This Situation I Delelted The Lines Takes The Length Of Array.
+{
+
+    // cout  << "\nEnter Number Of Elements: \n";
+    // cin >> ArrayLength;
+
+
+    for(int i = 0; i < ArrayLength; i++)
+        arr[i] = RandomNumber(1, 100);
+
+}
+
+
+void PrintArray(int Array[100], int ArrayLength)
+{
+
+    cout << "\n";
+
+    for(int i = 0; i < ArrayLength; i++)
+    {
+
+        cout << Array[i] << " ";
+
+    }
+
+    cout << endl;
+
+}
+
+
+void SumOfTwoArrays(int arr1[100], int arr2[100], int arr3[100], int ArrayLength)
+{
+
+    for(int i = 0; i < ArrayLength; i++)
+    {
+        arr3[i] = arr1[i] + arr2[i]; 
+    }
+
+}
+
+int main()
+{
+    
+    srand((unsigned) time (NULL));
+
+    int arr1[100], arr2[100], 
+    ArrayLength = ReadPositiveNumber("Enter Array Length? "); 
+
+    // Fill Array One.
+    FillArrayWithRandomNumbers(arr1, ArrayLength);
+
+    // Fill Array Two.
+    FillArrayWithRandomNumbers(arr2, ArrayLength);
+
+
+    // This Array We Will Put The Sum Of Two Arrays In It.
+    int arr3[100];
+
+    // Sum Of Tow Arrays.
+    SumOfTwoArrays(arr1, arr2, arr3, ArrayLength);
+
+    // Print Array One.
+    cout << "\nElements Of Array One:";
+    PrintArray(arr1,  ArrayLength);
+
+    // Print Array Two.
+    cout << "\nElements Of Array Two: ";
+    PrintArray(arr2,  ArrayLength);
+
+    // Print The Result.
+    cout << "\nSum Of Arrays: ";
+    PrintArray(arr3,  ArrayLength);
+
+    
     return 0;
 }
