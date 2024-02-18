2f7e9ce683755b329b159de02f9d863a41fe364e
AmjadLap <amjad.sayed.private@gmail.com>
2024-02-18 21:04:46 +0200

    Finshed Project One :)

--- testTwo.cpp
+++ testTwo.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
-
+#include <string.h>
 
 using namespace std;
+
+
+<<<<<<< HEAD
+int main()
+{
+
+
+=======
+enum hi {hi = 1, HY =2 };
 
 
 int main()
 {
 
-
+    cout 
+    
+>>>>>>> 7fd7841 (Finshed Project One :))
 
 
 
