a34b73a549116a06c8958a7c7bd04b5a520ef175
AmjadPrivate <amjad.sayed.private@gmail.com>
2024-02-23 23:34:01 +0200

    Finshed All My Solution For Project Two.

--- Projects/projectTwo/My_Solution_For_Project.cpp
+++ Projects/projectTwo/My_Solution_For_Project.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
@@ -6,38 +5,36 @@
 
 using namespace std;
 
+
 enum enQuestionsLevel {Easy =1, Med =2, Hard =3, Mix =4};
 
-enum enOperationTypes {Add =1, Sub = 2, Mul =3, Div = 4, MixOf = 4};
+enum enOperationTypes {Add =1, Sub = 2, Mul =3, Div = 4, MixOf = 5};
 
-enum enCheckAnswer {True = 1, Wrong = 2};
-
-enum enPassOrFail {Pass = 1, Fail = 2};
+enum enAnswerIs {Right = 1, Wrong};
 
 struct stFinalResult
 {
     short NumberOfQuestions = 0;
-    enQuestionsLevel QuestionsLevel;
-    enOperationTypes OperationTypes;
-    short NumberOfRightAnswer;
-    short NumberOfWrongAnswer;
-
+    string QuestionLevel;
+    string OPType;
+    short NumberOfRightAnswer = 0;
+    short NumberOfWrongAnswer = 0;
 };
+
 
 struct stQuestionInfo
 {
-    short NumberOfQuestion = 0;
+
     short NumberOne = 0;
-    short NumberTwo = 0;
-    enQuestionsLevel QuestionsLevel;
-    enOperationTypes OperationTypes;
-    enCheckAnswer CheckAnswer;
-    char OT;
-    short Answer = 0;
-    short NumberOfRightAnswers = 0;
-    short NumberOfWrongAnswers = 0;
+    short NumberTwo = 0; 
+    enQuestionsLevel QuestionLevel;
+    enOperationTypes OPType;
+    float Answer = 0;
+    short NumberOfRightAnswer = 0;
+    short NumberOfWrongAnswer = 0;
 
 };
+
 
 int GetRandomNumber(short From, short To)
 {
@@ -47,14 +44,29 @@ int GetRandomNumber(short From, short To)
 }
 
 
+
+string Taps(short NumberOfTaps)
+{
+    string t = "";
+
+    for(int i = 1; i <= NumberOfTaps; i++)
+    {
+        t = t + "\t";
+        cout << t;
+    }
+
+    return t;
+}
+
+
 short ReadHowMayQuestions()
 {
     short Number;
 
     do 
     {
-
-        cout << "How Many Questions Do You Want To Answer 1 To 100: ";
+        cout << endl;
+        cout << Taps(2) << "How Many Questions Do You Want To Answer 1 To 100: ";
         cin >> Number;
 
     
@@ -66,13 +78,14 @@ short ReadHowMayQuestions()
 }
 
 
+
 enQuestionsLevel ReadQuestionsLevel()
 {
     short QL; // Questions Level
 
     do 
     {
-        cout << "Enter Questions Level [1] Easy, [2] Med, [3] Hard, [4]Mix ? ";
+        cout << Taps(2) <<"Enter Questions Level [1] Easy, [2] Med, [3] Hard, [4]Mix ? ";
         cin >> QL;
     
     }
@@ -88,7 +101,7 @@ enOperationTypes ReadOperationType()
 
     do 
     {
-        cout << "Enter Operation Type [1] Add, [2] Sub, [3] Mul, [4] Div [5] Mix ? ";
+        cout << Taps(2) << "Enter Operation Type [1] Add, [2] Sub, [3] Mul, [4] Div [5] Mix ? ";
         cin >> OT;
     
     }
@@ -98,34 +111,39 @@ enOperationTypes ReadOperationType()
 }
 
 
-// I Will Use This Function For Make Questions
- int GetNumberAccordingToDifficulty(enQuestionsLevel QuestionsLevel)
- {
-     switch (QuestionsLevel)
-     {
-         case enQuestionsLevel::Easy:
-         {
-             return GetRandomNumber(1,10);
-             break;
-         }
-         case enQuestionsLevel::Med:
-         {
-             return GetRandomNumber(10, 30);
-             break;
-         }
-         case enQuestionsLevel::Hard:
-         {
-             return GetRandomNumber(20, 60);
-            break;
-         }
-        default: //  Means Mix
-         {
-             return GetRandomNumber(6, 120);
-             break;
-         }
-     }
 
- }
+int GetNumberAccordingToDifficulty(enQuestionsLevel QuestionsLevel)
+{
+    switch (QuestionsLevel)
+    {
+        case enQuestionsLevel::Easy:
+        {
+            return GetRandomNumber(1,10);
+            break;
+        }
+        case enQuestionsLevel::Med:
+        {
+            return GetRandomNumber(10, 30);
+            break;
+        }
+        case enQuestionsLevel::Hard:
+        {
+            return GetRandomNumber(20, 120);
+        break;
+        }
+        case enQuestionsLevel::Mix:
+        {
+            return GetRandomNumber(1, 120);
+            break;
+        }
+        default: // If The Default Happened Means There Is Wrong In The Input
+        {
+            return 0;
+            break;
+        }
+    }
+
+}
 
 
  char OperationCharacter(enOperationTypes OT)
@@ -160,15 +178,18 @@ enOperationTypes ReadOperationType()
  }
 
 
-void QuestionStyle(short Num1, short Num2, char OT)
+
+void QuestionStyle(short NumberOne, short NumberTwo, char OPCharacter)
 {
-    cout << Num1 << endl;
-    cout << "\n" << Num2 << " " << OT << endl;
-    cout << "------------\n";
+
+    cout << Taps(2) << NumberOne << endl;
+    cout << endl << Taps(2) << NumberTwo << " " << OPCharacter << endl;
+    cout << endl << Taps(2) << "---------------\n";
+    
 }
 
 
-short SolveQuestion(short Num1, short Num2, enOperationTypes OT)
+float AnswerOfQuestion(short Num1, short Num2, enOperationTypes OT)
 {
 
     switch (OT)
@@ -190,113 +211,139 @@ short SolveQuestion(short Num1, short Num2, enOperationTypes OT)
         }
         case enOperationTypes::Div:
         {
-            return Num1 / Num2;
+            return float(Num1) / Num2;
             break;
         }
         default:
-            return 0; // Something Wrong.
+            return 0; // Wrong Input.
+
     }
 
 }
 
 
-enCheckAnswer CheckAnswer(short TheRightAnswer, short& UserAnswer)
-{
 
-    if(UserAnswer == TheRightAnswer)
+// This Function Is The Most Importent In App, This Will Make Answer And Solve The Answer And Compile The Right Answer And User Answer And Return Right Answer Or Wrong Answer Depend Of User Input 
+enAnswerIs MakeQuestion(stQuestionInfo QInfo)
+{
+    float RightAnswer = 0; 
+
+    // In Case The Operation Type Is Mix
+    if(QInfo.OPType == enOperationTypes::MixOf)
     {
-        return enCheckAnswer::True;
+        QInfo.OPType = (enOperationTypes)GetRandomNumber(1, 4);
+    }
+
+    // Show The Question On The Screen.
+    QuestionStyle(QInfo.NumberOne, QInfo.NumberTwo, OperationCharacter(QInfo.OPType));
+
+    // Take Answer From User.
+    cout << Taps(2);
+    cin >> QInfo.Answer;
+
+    // This Is The Right Answer.
+    RightAnswer = AnswerOfQuestion(QInfo.NumberOne, QInfo.NumberTwo, QInfo.OPType);
+
+
+    if(QInfo.Answer == RightAnswer)
+    {
+        cout << endl << Taps(2) << "Right Answer :--) \n";
+        return enAnswerIs::Right;
     }
     else
     {
-        UserAnswer = TheRightAnswer;
-        return enCheckAnswer::Wrong;
-    }
+        cout << endl << Taps(2) << "Wrong Answer :--( \n";
+        cout << endl << Taps(2) << "The Right Answer Is " << RightAnswer << endl;
 
-}
-
-void AnswerOfQuestion(short TheRightAnswer, enCheckAnswer CheckAnswer)
-{
-    if(CheckAnswer == enCheckAnswer::True)
-    {
-        cout <<"\nRight Answer :--) \n";
-    }
-    else
-    {
-        cout <<"\nWrong Answer :--( \n";
-        cout << "The Right Answer Is " << TheRightAnswer << endl;
-
+        return enAnswerIs::Wrong;
     }
 
 
 }
 
 
-stFinalResult FillFinalResult(stQuestionInfo QuestionsInfo)
+
+string NameOfLevels(enQuestionsLevel QL)
 {
+    string LevelName[4] = {"Easy", "Med", "Hard", "Mix"};
+
+    return LevelName[QL - 1];
+}
+
+
+string NameOfOperation(enOperationTypes OT)
+{
+    string OperationNmaes[5] = {"Add", "Sub", "Mul", "Div", "Mix"};
+
+    return OperationNmaes[OT - 1];
+}
+
+
+void FillFinalResult(stQuestionInfo QInfo, stFinalResult& FinalResult)
+{
+
+    FinalResult.NumberOfQuestions = QInfo.NumberOfRightAnswer + QInfo.NumberOfWrongAnswer;
+    FinalResult.QuestionLevel = NameOfLevels(QInfo.QuestionLevel) ;
+    FinalResult.OPType = NameOfOperation(QInfo.OPType);
+    FinalResult.NumberOfRightAnswer = QInfo.NumberOfRightAnswer;
+    FinalResult.NumberOfWrongAnswer = QInfo.NumberOfWrongAnswer;
+
+}
+
+
+
+stFinalResult PlayMathGame(short QuestionsNumber)
+{
+
+    /*
+        Q = Question.
+        OPType = Operation Type.
+
+    */
+
     stFinalResult FinalResult;
 
-    FinalResult.NumberOfQuestions = QuestionsInfo.NumberOfQuestion;
-    FinalResult.QuestionsLevel = QuestionsInfo.QuestionsLevel;
-    FinalResult.OperationTypes = QuestionsInfo.OperationTypes;
-    FinalResult.NumberOfRightAnswer = QuestionsInfo.NumberOfRightAnswers;
-    FinalResult.NumberOfWrongAnswer = QuestionsInfo.NumberOfWrongAnswers;
-
-    return FinalResult;
-}
+    stQuestionInfo QInfo;
 
 
-stFinalResult PlayMathGame(short QuestionNumbers)
-{
-
-    stQuestionInfo QuestionInfo;
-
-    QuestionInfo.NumberOfQuestion = QuestionNumbers;
-    QuestionInfo.QuestionsLevel = ReadQuestionsLevel();
-    QuestionInfo.OperationTypes = ReadOperationType();
+    QInfo.QuestionLevel = ReadQuestionsLevel();
+    QInfo.OPType = ReadOperationType();
 
 
 
-    for(int QuestionNum  = 1; QuestionNum <= QuestionNumbers; QuestionNum++)
+    for(int QNumber = 1; QNumber <= QuestionsNumber; QNumber++)
     {
-        cout << "\nQuestions [" << QuestionNum << "/" << QuestionNumbers << "]" << endl;
 
-        // If Operation Type Is Mix
-        if(QuestionInfo.OperationTypes == enOperationTypes::MixOf)
+        cout << endl << Taps(2) << "Question [" << QNumber << "/" << QuestionsNumber << "]\n" << endl; // Number Of Questions
+
+
+        // Fill Two Numbers Random Numbers According To Difficulty
+        QInfo.NumberOne = GetNumberAccordingToDifficulty(QInfo.QuestionLevel);
+        QInfo.NumberTwo = GetNumberAccordingToDifficulty(QInfo.QuestionLevel);
+
+
+        // Make Question And Take Answer From User And Solve The Quesition And Compile The Answers If The User Answer Same Like Computer Answer Return Right Answer, Otherwise Return Wrong Answer
+        if(MakeQuestion(QInfo) == enAnswerIs::Right)
         {
-            enOperationTypes OPType;
-
-            OPType = (enOperationTypes)GetRandomNumber(1, 4);
+            QInfo.NumberOfRightAnswer++;
+        }
+        else
+        {
+            QInfo.NumberOfWrongAnswer++;
         }
 
 
-
-        QuestionInfo.NumberOne = GetNumberAccordingToDifficulty(QuestionInfo.QuestionsLevel); // Get Random Number According Thd Difficulty
-        QuestionInfo.NumberTwo = GetNumberAccordingToDifficulty(QuestionInfo.QuestionsLevel); // Get Random Number According Thd Difficulty
-        QuestionInfo.OT = OperationCharacter(QuestionInfo.OperationTypes); // Character Of Operation.
-
-        QuestionStyle(QuestionInfo.NumberOne, QuestionInfo.NumberTwo, QuestionInfo.OT);
-        cin >> QuestionInfo.Answer;
-
-        QuestionInfo.CheckAnswer = CheckAnswer(SolveQuestion(QuestionInfo.NumberOne, QuestionInfo.NumberTwo, QuestionInfo.OperationTypes), QuestionInfo.Answer);
-        AnswerOfQuestion(QuestionInfo.Answer, QuestionInfo.CheckAnswer);
-
-
-        if(QuestionInfo.CheckAnswer == enCheckAnswer::True)
-            QuestionInfo.NumberOfRightAnswers++;
-        else
-            QuestionInfo.NumberOfWrongAnswers++;
-
     }
 
-    return FillFinalResult(QuestionInfo);
+    FillFinalResult(QInfo, FinalResult);
+
+    return FinalResult;
+
 }
 
 
 
-
-
-string FinalResultIs(short RightAnswers, short WrongAnswers)
+string PassOrFail(short RightAnswers, short WrongAnswers)
 {
     if(RightAnswers > WrongAnswers)
     {
@@ -312,58 +359,35 @@ string FinalResultIs(short RightAnswers, short WrongAnswers)
     }
 }
 
-string NameOfLevels(enQuestionsLevel QL)
-{
-    string LevelName[4] = {"Easy", "Med", "Hard", "Mix"};
-
-    return LevelName[QL - 1];
-}
-
-string NameOfOperation(enOperationTypes OT)
-{
-    string OperationNmaes[5] = {"Add", "Sub", "Mul", "Div", "Mix"};
-
-    return OperationNmaes[OT - 1];
-}
-
-string Taps(short NumberOfTaps)
-{
-    string t = "";
-
-    for(int i = 1; i <= NumberOfTaps; i++)
-    {
-        t = t + "\t";
-        cout << t;
-    }
-
-    return t;
-}
 
 void PrintFinalResult(stFinalResult FinalResult)
 {
-    cout << Taps(3) << "---------------------\n";
 
-    cout << Taps(3) << FinalResultIs(FinalResult.NumberOfRightAnswer, FinalResult.NumberOfWrongAnswer) << endl;
+    cout << endl;
 
-    cout << Taps(3)  <<  "---------------------\n";
+    cout << endl << Taps(2) << "---------------------\n";
 
-    cout << Taps(3) << "Number Of Questions  : " << FinalResult.NumberOfQuestions << endl;
-    cout << Taps(3) << "Questions Level        : " << NameOfLevels(FinalResult.QuestionsLevel) << endl;
-    cout << Taps(3) << "OP Type                : " << NameOfOperation(FinalResult.OperationTypes)<< endl;
-    cout << Taps(3)  << "Number Of Write Answer : " << FinalResult.NumberOfRightAnswer << endl;
-    cout << Taps(3) << "Number Of Wrong Answer : " << FinalResult.NumberOfWrongAnswer << endl;
+    cout << Taps(2) << PassOrFail(FinalResult.NumberOfRightAnswer, FinalResult.NumberOfWrongAnswer) << endl;
 
-    cout << Taps(3) << "---------------------\n";
+    cout << Taps(2)  <<  "---------------------\n";
 
+    cout << Taps(2) << "Number Of Questions     : " << FinalResult.NumberOfQuestions << endl; 
+    cout << Taps(2) << "Questions Level         : " << FinalResult.QuestionLevel << endl;
+    cout << Taps(2) << "OP Type                 : " << FinalResult.OPType << endl;
+    cout << Taps(2) << "Number Of Write Answer  : " << FinalResult.NumberOfRightAnswer << endl;
+    cout << Taps(2) << "Number Of Wrong Answer  : " << FinalResult.NumberOfWrongAnswer << endl;
+    cout << Taps(2) << "---------------------\n";
 
 
 }
+
+
 
 bool PlayAgain()
 {
     char Choice = 'Y';
 
-    cout << Taps(3)<< "Do You Want Play Again Y/N ";
+    cout << Taps(2)<< "Do You Want Play Again Y/N ";
     cin >> Choice;
 
 
@@ -373,7 +397,6 @@ bool PlayAgain()
 
 void StartGame()
 {
-
 
     do
     {
@@ -385,9 +408,7 @@ void StartGame()
     while (PlayAgain());
 
 
-
 }
-
 
 
 int main()
---
+++ Projects/projectTwo/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    srand((unsigned) time (NULL));
+
+
+    return 0;
+}
